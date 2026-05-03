39500f0c489b662dfbc57d9468b2a7e37f6e7904
ekaterinaekaterinicheva <1ekaterina.kovaleva@gmail.com>
2026-05-03 17:19:42 +0200

    Enhance main view to handle new submissions and redirect to dashboard

--- monitoring_dashboard/views.py
+++ monitoring_dashboard/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Statistic, DataItem
 
 def main(request):
     qs = Statistic.objects.all()
+    if request.method == "POST":
+        new_stat = request.POST.get("new-statistic")
+        obj, _ = Statistic.objects.get_or_create(name=new_stat)
+        return redirect("monitoring_dashboard:dashboard", obj.slug)
     return render(request, "monitoring_dashboard/main.html", {'qs': qs})
 
 def dashboard(request, slug):
-    return render(request, "monitoring_dashboard/dashboard.html", {'dashboard_slug': slug})
+    obj = get_object_or_404(Statistic, slug=slug)
+    return render(request, "monitoring_dashboard/dashboard.html", {
+        'name': obj.name,
+        'slug': obj.slug,
+        'data': obj.data
+        })
