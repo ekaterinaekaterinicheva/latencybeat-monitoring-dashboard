4229106bb091d1eb60ec0e5e3c59446bf6409486
ekaterinaekaterinicheva <1ekaterina.kovaleva@gmail.com>
2026-05-03 15:35:26 +0200

    Add WebSocket connection and message handling in dashboard.js

--- monitoring_dashboard/static/stats/dashboard.js
+++ monitoring_dashboard/static/stats/dashboard.js
@@ -1,1 +1,13 @@
 console.log("Hello, World!!");
+
+const socket = new WebSocket("ws:// + window.location.host + /ws/test-stat/");
+
+socket.onmessage = function (e) {
+    console.log('Server: ' + e.data);
+};
+
+socket.onopen = function (e) {
+    socket.send(JSON.stringify({
+        'message': 'Hello from client!',
+    }));
+};
