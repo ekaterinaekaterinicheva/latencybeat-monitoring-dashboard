9a2874d46e8a1e91a6d2ec9e51ab1af260889b97
ekaterinaekaterinicheva <1ekaterina.kovaleva@gmail.com>
2026-05-03 22:48:30 +0200

    Refactor routing.py to improve readability; enhance comments for websocket URL patterns

--- monitoring_dashboard/routing.py
+++ monitoring_dashboard/routing.py
@@ -2,6 +2,11 @@ from django.urls import re_path
 from typing import Any, cast
 from .consumers import DashboardConsumer
 
+# This list tells Channels which consumer to trigger based on the URL
 websocket_urlpatterns = [
-    re_path(r'^ws/(?P<dashboard_slug>[\w-]+)/$', cast(Any, DashboardConsumer.as_asgi())),
+    # A regex path is used to capture the 'dashboard_slug' from the URL
+    # Ex.: ws://127.0.0.1:8000/ws/server-1/
+    re_path(r'^ws/(?P<dashboard_slug>[\w-]+)/$',
+            cast(Any, DashboardConsumer.as_asgi()) # cast(Any) prevents Pylance from complaining about as_asgi()
+    ),
 ]
