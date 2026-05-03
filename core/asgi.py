59d5e16dd84ca02dcc0be20c76c2ac8bc969325e
ekaterinaekaterinicheva <1ekaterina.kovaleva@gmail.com>
2026-05-03 22:41:47 +0200

    Refactor ASGI configuration for improved readability; reorganize import statements and enhance application entry point definition

--- core/asgi.py
+++ core/asgi.py
@@ -8,22 +8,32 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
+# Set settings and initialize the Django ASGI application 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 django_asgi_app = get_asgi_application()
 
+# Import Channels components
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
 import monitoring_dashboard.routing
 
+# Define the main application entry point
 application = ProtocolTypeRouter(
     {
+        # Standard HTTP traffic handled by Django
         "http": django_asgi_app,
+
+        # WebSocket traffic handled by Channels
         "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(monitoring_dashboard.routing.websocket_urlpatterns))
+            AuthMiddlewareStack(
+                URLRouter(
+                    monitoring_dashboard.routing.websocket_urlpatterns
+                )
+            )
         ),
     }
 )
